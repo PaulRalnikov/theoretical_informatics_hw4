99149dad2c7ae66d271e3208d2a8515aeb51b0ba
DungeonMaster <st106332@student.spbu.ru>
2023-10-08 23:46:34 +0300

    Added main, done minimize function

--- task4/src/main/kotlin/DFA.kt
+++ task4/src/main/kotlin/DFA.kt
@@ -1,4 +1,5 @@
 import java.io.File
+import kotlin.math.max
 
 class DFA(val n : Int = 0,
           val m : Int = 0,
@@ -7,9 +8,99 @@ class DFA(val n : Int = 0,
           val go : Array<Array<Int>> = emptyArray()
 ) {
 
-    fun minimize() : DFA {
+    inner class Category (val type : Array<Int>) {
+        val typeContent : MutableList<MutableList<Int>>
+        var nextFreeType : Int
+        val freeTypes : MutableList<Int>
+        init {
+            typeContent = MutableList (n) { mutableListOf()}
+            nextFreeType = 0
+            freeTypes = mutableListOf()
 
-        return DFA()
+            for (i in 0 until n) {
+                typeContent[type[i]].add(i)
+                nextFreeType = max(nextFreeType, type[i] + 1)
+            }
+
+            for (i in 0 until nextFreeType) {
+                if (typeContent[i].isEmpty()) freeTypes.add(i)
+            }
+        }
+
+        fun addType(content : List<Int>) {
+            val numType : Int
+            if (freeTypes.isNotEmpty()) numType = freeTypes.removeLast()
+            else {
+                numType = nextFreeType++
+                typeContent.add(mutableListOf())
+            }
+            typeContent[numType] = content.toMutableList()
+            content.forEach { type[it] = numType }
+        }
+
+        fun eraseType(numType : Int) {
+            typeContent[numType].forEach { type[it] = -1 }
+            typeContent[numType] = mutableListOf()
+            freeTypes.add(numType)
+        }
+
+        fun getType(v : Int) = type[v]
+
+        fun compress()  {
+            TODO()
+        }
+    }
+
+    fun minimize() : DFA {
+        val category = Category(Array(n) {
+            if (it in finish) 1 else 0
+        })
+        var flag = true
+        while (flag) {
+            flag = false
+            for (type in category.typeContent.withIndex()) {
+                var deletedType = false
+                for (letter in 0 until m) {
+                    val outgoingTypes : Set<Int> = type.value.map {v -> category.getType(go[v][letter])}.toSet()
+                    if (outgoingTypes.size == 1) continue
+                    deletedType = true
+
+                    //numeratedOutgoingTypes says number of new class by type of the vertex
+                    val numeratedOutgoingTypes : Map<Int, Int> = outgoingTypes.mapIndexed {idx, it -> it to idx}.toMap()
+                    val countNewTypes = numeratedOutgoingTypes.size
+                    val newTypes = List<MutableList<Int>> (countNewTypes) { mutableListOf()}
+
+                    type.value.forEach {v ->
+                        newTypes[
+                            numeratedOutgoingTypes.getOrDefault(
+                                category.getType(go[v][letter]),
+                                -1)
+                        ].add(v)
+                    }
+
+                    category.eraseType(type.index)
+                    newTypes.forEach { category.addType(it) }
+                    break
+                }
+                if (deletedType) {
+                    flag = true
+                    break
+                }
+            }
+        }
+        category.compress()
+        val newN = category.nextFreeType
+        val newM = m
+        val newStart = category.getType(start)
+        val newFinish = finish.map { category.getType(it) }.toSet()
+        val newGo = Array(newN) { v ->
+            Array (newM) {letter ->
+                category.getType(
+                    go[category.typeContent[v][0]][letter]
+                )
+            }
+        }
+        return DFA(newN, newM, newStart, newFinish, newGo)
     }
 
     fun printToFile(outputFile : File) {
--- task4/src/main/kotlin/main.kt
+++ task4/src/main/kotlin/main.kt
@@ -1,3 +1,10 @@
-fun main() {
+import java.io.File
 
+fun main() {
+    println("enter relative path (from directory with project) to file with input data:")
+    val inputPath = readlnOrNull().toString()
+    println("enter relative path to result file:")
+    val resultPath = readlnOrNull().toString()
+    val dfa = readDfa(File(inputPath))
+    dfa.minimize().printToFile(File(resultPath))
 }
